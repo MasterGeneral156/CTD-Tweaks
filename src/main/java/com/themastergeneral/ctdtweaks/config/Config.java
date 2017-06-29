9aa6b00ec8c0e8c4ab558025c09d59c5709b8e92
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-06-28 23:26:32 -0400

    Cleaned up config

--- src/main/java/com/themastergeneral/ctdtweaks/config/Config.java
+++ src/main/java/com/themastergeneral/ctdtweaks/config/Config.java
@@ -13,20 +13,8 @@ public class Config
     private static final String CATEGORY_GENERAL = "General";
 
     // This values below you can access elsewhere in your mod:
-    public static boolean disableBaubles = false;
-    public static boolean disableCarrot = false;
-    public static boolean disableWitherFuel = false;
-    public static boolean disableWitherFuelBlock = false;
-    public static boolean disableBossDrops = false;
-    public static boolean disableGagItems = false;
-    public static boolean disableCores = false;
-    public static boolean disableNightVision = false;
-    public static boolean disableEnchantedGold = false;
-    public static boolean disableElytra = false;
-    public static boolean disableJei = false;
-    public static String ModVersion = Main.VERSION+"-1.10.2";
+    public static String ModVersion = Main.VERSION+"-1.12";
     public static int coreCombatDurability = 64;
-    public static int coreFlightDurability = 128;
     public static int witherFuelBurn = 56000;
 
     // Call this from CommonProxy.preInit(). It will create our config if it doesn't
@@ -55,19 +43,7 @@ public class Config
     {
         cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration for the CTD Tweaks mod.");
         ModVersion = cfg.getString("ModVersion", CATEGORY_GENERAL, ModVersion, "Internal. Don't need to mess with this.");
-        disableBaubles = cfg.getBoolean("disableBauble", CATEGORY_GENERAL, disableBaubles, "Set to true to remove the Baubles items.");
-        disableCarrot = cfg.getBoolean("disableCarrot", CATEGORY_GENERAL, disableCarrot, "Set to true to remove the Steamed Carrot.");
-        disableCores = cfg.getBoolean("disableCores", CATEGORY_GENERAL, disableCores, "Set to true to remove the cores items. (Ex. Combat Core)");
-        disableElytra = cfg.getBoolean("disableElytra", CATEGORY_GENERAL, disableElytra, "Set to true to remove the crafting recipe for the Elytra.");
-        disableNightVision = cfg.getBoolean("disableNightVision ", CATEGORY_GENERAL, disableNightVision , "Set to true to remove the night vision goggles.");
-        disableBossDrops = cfg.getBoolean("disableBossDrops", CATEGORY_GENERAL, disableBossDrops, "Set to true to remove the ability to craft boss drops. (Dragon Egg, Nether Star, etc.)");
-        disableWitherFuel = cfg.getBoolean("disableWitherFuel", CATEGORY_GENERAL, disableWitherFuel, "Set to true to remove the Wither Fuel item.");
-        disableGagItems = cfg.getBoolean("disableGagItems", CATEGORY_GENERAL, disableGagItems, "Set to true to remove gag items from the mod.");
-        disableEnchantedGold = cfg.getBoolean("disableEnchantedGold", CATEGORY_GENERAL, disableEnchantedGold, "Set to true to remove Enchanted Gold.");
-        disableWitherFuelBlock = cfg.getBoolean("disableWitherFuelBlock", CATEGORY_GENERAL, disableWitherFuelBlock, "Set to true to remove the Wither Fuel block.");
-        disableJei = cfg.getBoolean("disableJei", CATEGORY_GENERAL, disableJei, "Set to true to remove Just Enough Items integration.");
         coreCombatDurability = cfg.getInt("coreCombatDurability", CATEGORY_GENERAL, coreCombatDurability, 1, 4096, "Uses you can get out of the Combat Core before it breaks.");
-        coreFlightDurability = cfg.getInt("coreFlightDurability", CATEGORY_GENERAL, coreFlightDurability, 1, 4096, "Uses you can get out of the Flight Core before it breaks.");
         witherFuelBurn = cfg.getInt("Wither Fuel Burn Time", CATEGORY_GENERAL, witherFuelBurn, 1, 2147483647, "How many ticks will Wither Fuel burn for? (Block is X10)");
     }
 }
