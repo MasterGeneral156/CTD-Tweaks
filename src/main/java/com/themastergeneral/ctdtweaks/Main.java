ff632b935892a57529a50b46f55432e3875abfdd
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-07-01 14:14:12 -0400

    1.5.2 build

--- src/main/java/com/themastergeneral/ctdtweaks/Main.java
+++ src/main/java/com/themastergeneral/ctdtweaks/Main.java
@@ -21,7 +21,7 @@ public class Main
 	public static boolean baublesLoaded = false;
     public static final String MODID = "ctdtweaks";
     public static final String MODNAME = "CTD Tweaks";
-    public static final String VERSION = "1.5.1";
+    public static final String VERSION = "1.5.2";
     public static final String DEPENDENCIES = "required-after:ctdcore@[1.1.3,]";
     public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Tweaks.json";
     
