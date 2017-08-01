69f54bceea3d072f731390d0dc80d25a3fff947d
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-08-01 13:50:37 -0400

    Removed events

--- src/main/java/com/themastergeneral/ctdtweaks/proxy/CommonProxy.java
+++ src/main/java/com/themastergeneral/ctdtweaks/proxy/CommonProxy.java
@@ -13,9 +13,6 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import com.themastergeneral.ctdtweaks.blocks.ModBlocks;
 import com.themastergeneral.ctdtweaks.config.Config;
-import com.themastergeneral.ctdtweaks.events.FlightEvent;
-import com.themastergeneral.ctdtweaks.events.HasteEvent;
-import com.themastergeneral.ctdtweaks.events.SpeedEvent;
 import com.themastergeneral.ctdtweaks.handlers.Crafting;
 import com.themastergeneral.ctdtweaks.handlers.Fuel;
 import com.themastergeneral.ctdtweaks.handlers.OreDict;
@@ -27,7 +24,7 @@ public class CommonProxy
 	public void preInit(FMLPreInitializationEvent e) 
     {
 		File directory = e.getModConfigurationDirectory();
-		config = new Configuration(new File(directory.getPath(), "CTD/CTDTweaks.cfg"));
+		config = new Configuration(new File(directory.getPath(), "ctd/ctdTweaks.cfg"));
 		Config.readConfig();
     	ModBlocks.init();
     	ModItems.init();
@@ -37,9 +34,6 @@ public class CommonProxy
     public void init(FMLInitializationEvent e) 
     {
     	Crafting.addRecipes();
-    	MinecraftForge.EVENT_BUS.register(new FlightEvent());
-    	MinecraftForge.EVENT_BUS.register(new HasteEvent());
-    	MinecraftForge.EVENT_BUS.register(new SpeedEvent());
     }
     public void postInit(FMLPostInitializationEvent e) 
     {
