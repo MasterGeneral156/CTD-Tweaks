c7e052827ea28c3a90e2ccf7d9049b0bb5ddb906
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-08-01 13:49:15 -0400

    Move BasicRing to here

---
+++ src/main/java/com/themastergeneral/ctdtweaks/items/baubles/BasicRing.java
@@ -0,0 +1,14 @@
+package com.themastergeneral.ctdtweaks.items.baubles;
+
+import com.themastergeneral.ctdtweaks.items.BasicItem;
+
+import net.minecraft.item.ItemStack;
+
+public class BasicRing extends BasicBauble {
+
+	public BasicRing(String name, String modid) 
+	{
+		super(name, modid);
+		this.maxStackSize=1;
+	}
+}
