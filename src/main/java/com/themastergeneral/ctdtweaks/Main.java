e7292e4f1803531ababa056b237653f81fdbe3c6
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-08-01 13:51:04 -0400

    Require Baubles and load after it

--- src/main/java/com/themastergeneral/ctdtweaks/Main.java
+++ src/main/java/com/themastergeneral/ctdtweaks/Main.java
@@ -20,8 +20,8 @@ public class Main
 {
     public static final String MODID = "ctdtweaks";
     public static final String MODNAME = "CTD Tweaks";
-    public static final String VERSION = "1.5.4";
-    public static final String DEPENDENCIES = "required-after:ctdcore@[1.1.6,]";
+    public static final String VERSION = "1.5.5";
+    public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.1.6,]";
     public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Tweaks.json";
     
     public static final CreativeTab creativeTab = new CreativeTab();
