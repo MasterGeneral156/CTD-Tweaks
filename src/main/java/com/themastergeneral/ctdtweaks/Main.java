931b7cc7694f3fda0dadeb59b8db7c707f6b7bad
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-09-24 17:16:31 -0400

    1.6

--- src/main/java/com/themastergeneral/ctdtweaks/Main.java
+++ src/main/java/com/themastergeneral/ctdtweaks/Main.java
@@ -19,8 +19,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Main {
 	public static final String MODID = "ctdtweaks";
 	public static final String MODNAME = "CTD Tweaks";
-	public static final String VERSION = "1.5.11";
-	public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.1.6,]";
+	public static final String VERSION = "1.6";
+	public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.1.6,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Tweaks.json";
 	public static final String MCVersion = "1.12.2";
 
