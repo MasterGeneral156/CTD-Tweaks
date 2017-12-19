cbfbc10fbeeafbbdf6265eb9cdf160a27c250a0f
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2017-12-18 21:14:23 -0500

    Add bauble type ring to ring base class

--- src/main/java/com/themastergeneral/ctdtweaks/items/baubles/BasicRing.java
+++ src/main/java/com/themastergeneral/ctdtweaks/items/baubles/BasicRing.java
@@ -1,5 +1,7 @@
 package com.themastergeneral.ctdtweaks.items.baubles;
 
+import baubles.api.BaubleType;
+
 import com.themastergeneral.ctdtweaks.items.BasicItem;
 
 import net.minecraft.item.ItemStack;
@@ -9,5 +11,10 @@ public class BasicRing extends BasicBauble {
 	public BasicRing(String name, String modid) {
 		super(name, modid);
 		this.maxStackSize = 1;
+	}
+	
+	@Override
+	public BaubleType getBaubleType(ItemStack arg0) {
+		return BaubleType.RING;
 	}
 }
