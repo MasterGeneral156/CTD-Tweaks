4e7c87ee2c9efba1ac93b7cee6b21bd6acba5af7
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-02-25 21:47:31 -0500

    1.8.3

--- src/main/java/com/themastergeneral/ctdtweaks/CTDTweaks.java
+++ src/main/java/com/themastergeneral/ctdtweaks/CTDTweaks.java
@@ -21,11 +21,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CTDTweaks {
 	public static final String MODID = "ctdtweaks";
 	public static final String MODNAME = "CTD Tweaks";
-	public static final String VERSION = "1.8.2";
+	public static final String VERSION = "1.8.3";
 	public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.2.3,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Tweaks.json";
 	public static final String MCVersion = "1.12.2";
-	public static final String FingerPrint = "441b509a0f58a0ef41aca8daf1be20d96287635e";
+	public static final String FingerPrint = "1cd8befc36d6dedc5601d77a013f43afc71f899f";
 
 	public static final CreativeTab creativeTab = new CreativeTab();
 
