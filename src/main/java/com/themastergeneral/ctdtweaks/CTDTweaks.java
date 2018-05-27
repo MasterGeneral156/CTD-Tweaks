8c6c5e6236827cfe28357dc3418183427b551515
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-05-27 14:34:19 -0400

    CTD Core 1.3.4 required

--- src/main/java/com/themastergeneral/ctdtweaks/CTDTweaks.java
+++ src/main/java/com/themastergeneral/ctdtweaks/CTDTweaks.java
@@ -22,7 +22,7 @@ public class CTDTweaks {
 	public static final String MODID = "ctdtweaks";
 	public static final String MODNAME = "CTD Tweaks";
 	public static final String VERSION = "1.9.2";
-	public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.3.3,];";
+	public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.3.4,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Tweaks.json";
 	public static final String MCVersion = "1.12.2";
 	public static final String FingerPrint = "5101015479fe39f20b47f365472250d312a50a57";
