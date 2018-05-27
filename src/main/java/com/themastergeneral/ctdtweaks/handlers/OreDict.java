f8d23ce2003a693d06efa3d6da013298fc70271d
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-05-27 15:58:08 -0400

    Add to the ore dictionary!

--- src/main/java/com/themastergeneral/ctdtweaks/handlers/OreDict.java
+++ src/main/java/com/themastergeneral/ctdtweaks/handlers/OreDict.java
@@ -9,5 +9,14 @@ import net.minecraftforge.oredict.OreDictionary;
 public class OreDict {
 	public static void InitOre() {
 		OreDictionary.registerOre("concrete", ModBlocks.concrete);
+		OreDictionary.registerOre("obsidian", ModBlocks.glowingobsidian);
+		OreDictionary.registerOre("glowstone", ModBlocks.glowingobsidian);
+		OreDictionary.registerOre("blockGlassBlack", ModBlocks.gothicglass);
+		OreDictionary.registerOre("blockGlass", ModBlocks.gothicglass);
+		OreDictionary.registerOre("ingotEnchantedGold", ModItems.enchantedgoldingot);
+		OreDictionary.registerOre("ingotWitherFuel", ModItems.fuelwither);
+		OreDictionary.registerOre("cropCarrot", ModItems.steamedcarrot);
+		OreDictionary.registerOre("soulsand", ModBlocks.thiccsoulsand);
+		OreDictionary.registerOre("blockSoulSand", ModItems.steamedcarrot);
 	}
 }
