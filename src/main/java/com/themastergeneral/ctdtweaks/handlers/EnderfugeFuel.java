f0fbae74d7cb0b5cc93adb5b49496912da88eb99
MasterGeneral156 <ryan.roach.1997@hotmail.com>
2018-05-27 14:36:38 -0400

    This way, in fact.

--- src/main/java/com/themastergeneral/ctdtweaks/handlers/EnderfugeFuel.java
+++ src/main/java/com/themastergeneral/ctdtweaks/handlers/EnderfugeFuel.java
@@ -2,16 +2,16 @@ package com.themastergeneral.ctdtweaks.handlers;
 
 import net.minecraft.item.ItemStack;
 
-import com.themastergeneral.ctdcore.imc.Enderfuge;
+import com.themastergeneral.ctdcore.imc.IMCHelper;
 import com.themastergeneral.ctdtweaks.blocks.ModBlocks;
 import com.themastergeneral.ctdtweaks.config.Config;
 import com.themastergeneral.ctdtweaks.items.ModItems;
 
 public class EnderfugeFuel {
 	public static void init() {
-		Enderfuge.addFuel(new ItemStack(ModBlocks.fuelwitherblock),
+		IMCHelper.addEnderfugeFuel(new ItemStack(ModBlocks.fuelwitherblock),
 				Config.witherFuelBurn);
-		Enderfuge.addFuel(new ItemStack(ModItems.fuelwither),
+		IMCHelper.addEnderfugeFuel(new ItemStack(ModItems.fuelwither),
 				Config.witherFuelBurn / 10);
 	}
 }
