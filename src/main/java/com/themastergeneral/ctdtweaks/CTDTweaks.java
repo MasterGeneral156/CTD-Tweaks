853aa5991f41be6fe7b9e9df36f4b3a950238d27
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-07-29 21:31:30 -0400

    1.10.1 build

--- src/main/java/com/themastergeneral/ctdtweaks/CTDTweaks.java
+++ src/main/java/com/themastergeneral/ctdtweaks/CTDTweaks.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CTDTweaks {
 	public static final String MODID = "ctdtweaks";
 	public static final String MODNAME = "CTD Tweaks";
-	public static final String VERSION = "1.10.0";
+	public static final String VERSION = "1.10.1";
 	public static final String DEPENDENCIES = "required-after:baubles;required-after:ctdcore@[1.4.2,];";
 	public static final String updateJSON = "https://raw.githubusercontent.com/MasterGeneral156/Version/master/CTD-Tweaks.json";
 	public static final String MCVersion = "1.12.2";
