3a078fbb1caaa44ea44c47dd5aab93717ade30e0
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-07-17 13:13:41 -0400

    Removed unused imports

--- src/main/java/com/themastergeneral/ctdtweaks/items/HasteCuriosItem.java
+++ src/main/java/com/themastergeneral/ctdtweaks/items/HasteCuriosItem.java
@@ -6,8 +6,6 @@ import com.themastergeneral.ctdtweaks.CTDTweaks;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
