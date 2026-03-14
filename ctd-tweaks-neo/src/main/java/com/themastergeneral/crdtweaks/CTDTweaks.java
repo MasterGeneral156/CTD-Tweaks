471f8168da831e7db207b6e1c75516674f8c8c85
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2026-03-14 13:38:29 -0400

    Update CTDTweaks.java

--- ctd-tweaks-neo/src/main/java/com/themastergeneral/crdtweaks/CTDTweaks.java
+++ ctd-tweaks-neo/src/main/java/com/themastergeneral/crdtweaks/CTDTweaks.java
@@ -1,54 +1,33 @@
 package com.themastergeneral.crdtweaks;
 
-import com.themastergeneral.crdtweaks.items.ItemConstants;
 import com.themastergeneral.crdtweaks.registries.ItemRegistry;
 import net.minecraft.world.item.*;
-import net.neoforged.fml.InterModComms;
-import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.SlotTypeMessage;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+
 @Mod(CTDTweaks.MODID)
 public class CTDTweaks {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "ctdtweaks";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "ctdtweaks" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "ctdtweaks" namespace
 
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "ctdtweaks" namespace
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "ctdtweaks:example_block", combining the namespace and path
     public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
 
     public CTDTweaks(IEventBus modEventBus, ModContainer modContainer) {
@@ -57,6 +36,7 @@ public class CTDTweaks {
         BLOCKS.register(modEventBus);
         ItemRegistry.ITEMS.register(modEventBus);
         CREATIVE_MODE_TABS.register(modEventBus);
+
         //NeoForge.EVENT_BUS.register(this);
 
         modEventBus.addListener(this::addCreative);
@@ -67,7 +47,6 @@ public class CTDTweaks {
     private void commonSetup(FMLCommonSetupEvent event) {
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS)
         {
@@ -83,6 +62,5 @@ public class CTDTweaks {
         {
             event.accept(new ItemStack(ItemRegistry.pocket_cobble_generator.get()));
         }
-        LOGGER.info("Items filled in creative tabs.");
     }
 }
